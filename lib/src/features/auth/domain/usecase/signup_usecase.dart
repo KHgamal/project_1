5d6751c97f6c75850c59dda4ac028b75eb383330
AbdoWa7eed <abdo20132233@gmail.com>
2023-09-25 16:34:42 +0300

    Update domain layer

--- lib/src/features/auth/domain/usecase/signup_usecase.dart
+++ lib/src/features/auth/domain/usecase/signup_usecase.dart
@@ -4,13 +4,38 @@ import 'package:project_1/src/features/auth/domain/entities/driver_entity.dart';
 import 'package:project_1/src/features/auth/domain/repository/auth_repo.dart';
 
 class SignUpUseCase
-    extends UseCaseWithParams<DriverEntity, DriverEntity> {
+    extends UseCaseWithParams<DriverEntity, SignUpUseCaseParams> {
   final AuthRepository _authRepository;
   SignUpUseCase(this._authRepository);
   @override
-  ResultFuture<DriverEntity> call(DriverEntity p) async {
+  ResultFuture<DriverEntity> call(SignUpUseCaseParams p) async {
     return await _authRepository.signUp();
   }
 }
 
+class SignUpUseCaseParams {
+  final String fullName;
+  final String email;
+  final String password;
+  final String passwordConfirm;
+  final String phoneNumber;
+  final String idCard;
+  final String transferDoc;
+  final String playCard;
+  final String vehicleLicense;
+  final String driverLicense;
+  final String identityResidence;
 
+  const SignUpUseCaseParams(
+      {required this.fullName,
+        required this.email,
+        required this.password,
+        required this.passwordConfirm,
+        required this.phoneNumber,
+        required this.idCard,
+        required this.transferDoc,
+        required this.playCard,
+        required this.vehicleLicense,
+        required this.driverLicense,
+        required this.identityResidence});
+}
