733282e446e46ec720f980c7520cb952ffdda9e0
adib-Wageeh <adibwageeh5@gmail.com>
2023-09-26 19:10:12 +0300

    updating auth domain layer

--- lib/src/authentication/domain/repository/auth_repo.dart
+++
@@ -1,7 +0,0 @@
-import 'package:project_1/core/utils/typedefs.dart';
-import 'package:project_1/src/authentication/domain/entities/driver_entity.dart';
-
-abstract class AuthRepository {
-  ResultFuture<DriverEntity> signUp ();
-  ResultFuture<DriverEntity> signIn (String email,String password);
-}
